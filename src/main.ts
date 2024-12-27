1e2775a2d3ff8e1403cabd88ccdbf234403158f8
abhishek-jha-dev <jabhishek10142@gmail.com>
2024-12-27 16:29:38 +0530

    extend limit for company logo

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cors from 'cors';
+import { json, urlencoded } from 'express'; // Use these directly from Express
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -16,6 +17,9 @@ async function bootstrap() {
       credentials: true, // Enable credentials (e.g., cookies) for cross-origin requests
     }),
   );
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ limit: '10mb', extended: true }));
+
   await app.listen(4000 , ()=>{
     console.log('server will listen on port 4000');
   });
